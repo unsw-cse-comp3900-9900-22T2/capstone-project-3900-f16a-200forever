c292ce484e7c4634d6bcbc5d18930bf32b67c810
Yiwen Chen <z5241339@ad.unsw.edu.au>
2022-06-25 15:40:33 +0800

    login page finished

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,90 @@
-import React, { Component } from 'react';
+import { Button, Checkbox, Form, Input } from "antd";
+import { Typography } from "antd";
+import "../css/Login.css";
+const Login = () => {
+  const { Title } = Typography;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
 
-class Login extends Component {
-    state = {  } 
-    render() { 
-        return ("helloworld");
-    }
-}
- 
-export default Login ;
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div>
+      <Title className="login-title">Please log in </Title>
+
+      <Form
+        className="login-form"
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button
+            classname="login-form-button"
+            type="primary"
+            htmlType="submit"
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Register.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class register extends Component {
-    state = {  } 
-    render() { 
-        return ("hello world");
-    }
-}
- 
-export default register;
