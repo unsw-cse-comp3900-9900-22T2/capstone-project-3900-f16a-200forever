9c4bfc6f2c93aabbc004062e8a3c6b5d45896dcc
Yiwen Chen <z5241339@ad.unsw.edu.au>
2022-06-27 15:25:17 +0800

    update button layout

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,13 @@
 import { Button, Checkbox, Form, Input, notification } from "antd";
 import { Typography } from "antd";
-import axios from 'axios';
+import axios from "axios";
 import "../css/Login.css";
-
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
 const Login = ({ updateLoginStatus }) => {
   const { Title } = Typography;
   const onFinish = (values) => {
@@ -11,17 +16,18 @@ const Login = ({ updateLoginStatus }) => {
     // todo handle success
     // todo handle error
     // todo forget password?
-    axios.post('/url', {
-      email: values["email"],
-      password: values["password"]
-    })
-    .then(function (response) {
-      console.log(response);
-      updateLoginStatus(true);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    axios
+      .post("/url", {
+        email: values["email"],
+        password: values["password"],
+      })
+      .then(function (response) {
+        console.log(response);
+        updateLoginStatus(true);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -30,11 +36,11 @@ const Login = ({ updateLoginStatus }) => {
     notification.open({
       message: `Notification`,
       description:
-        'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        "This is the content of the notification. This is the content of the notification. This is the content of the notification.",
       placement: "top",
       duration: 3,
       onClick: () => {
-        console.log('Notification Clicked!');
+        console.log("Notification Clicked!");
       },
     });
   };
@@ -96,18 +102,20 @@ const Login = ({ updateLoginStatus }) => {
           <Checkbox>Remember me</Checkbox>
         </Form.Item> */}
 
-        <Form.Item
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
+        <Form.Item {...tailLayout}>
           <Button
             classname="login-form-button"
             type="primary"
             htmlType="submit"
           >
             LOGIN
+          </Button>
+          <Button
+            classname="login-forget-button"
+            
+            htmlType="button"
+          >
+            Forget Password
           </Button>
           {/* todo add forget button */}
         </Form.Item>
