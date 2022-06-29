55b0d866730ac344e4bad1dd5444aa1e85607849
Yu Kong <z5271686@ad.unsw.edu.au>
2022-06-29 23:50:01 +0800

    add MovieImage, add relationship

--- backend/movie/models/genre.py
+++ backend/movie/models/genre.py
@@ -5,6 +5,7 @@ class Genres(db.Model):
   __tablename__ = 't_genres'
   id = db.Column('id', db.String(32), primary_key=True)
   name = db.Column('name', db.String(256), nullable=False)
+  genre_movie = db.relationship('Movies', secondary='r_movie_genre', back_populates='movie_genre', lazy=True)
 
   def __repr__(self):
     return '<Genre id:{} name:{}>'.format(self.id, self.name)
@@ -15,8 +16,8 @@ class Genres(db.Model):
 
 class MovieGenre(db.Model):
   __tablename__ = 'r_movie_genre'
-  genre_id = db.Column('genre_id', db.Integer, db.ForeignKey('t_genres.id'))
-  movie_id = db.Column('movie_id', db.Integer, db.ForeignKey('t_movies.id'))
+  genre_id = db.Column('genre_id', db.Integer, db.ForeignKey('t_genres.id'), primary_key=True)
+  movie_id = db.Column('movie_id', db.Integer, db.ForeignKey('t_movies.id'), primary_key=True)
 
   def __repr__(self):
     return '<MovieGenre movie id: {} genren id: {}>'.format(self.movie_id, self.genre_id)
--- backend/movie/models/movie.py
+++ backend/movie/models/movie.py
@@ -16,6 +16,7 @@ class Movies(db.Model):
   movie_director_rel = db.relationship('Persons', secondary='r_movie_director', back_populates='director_movie_rel', lazy=True)
   movie_actor_rel = db.relationship('Persons', secondary='r_movie_actor', back_populates='actor_movie_rel', lazy=True)
   movie_genre = db.relationship('Genres', secondary='r_movie_genre', back_populates='genre_movie', lazy=True)
+  images = db.relationship('MovieImages', backref='movie', lazy=True)
 
   def __repr__(self):
     return '<Movie: {} {}>'.format(self.id, self.title)
@@ -33,13 +34,12 @@ class Movies(db.Model):
     self.rating_count = data['rating_count']
 
 class MovieImages(db.model):
-  __tablename__ = 'Movie_images'
-  id = db.Column('id', db.Integer(20), primary_key=True)
-  movie_id = db.Column('movie_id', db.Integer(20), db.ForeignKey('Movies.id'))
-  file_path = db.Column('file_path', db.String(400))
-  height = db.Column('height', db.Integer(20))
-  width = db.Column('width', db.Integer(20))
-  is_posters = db.Column('is_posters', db.Boolean)
+  __tablename__ = 't_movie_images'
+  id = db.Column('id', db.Integer, primary_key=True)
+  file_path = db.Column('file_path', db.String(256), nullable=False)
+  height = db.Column('height', db.Integer)
+  width = db.Column('width', db.Integer)
+  movie_id = db.Column('movie_id', db.Integer, db.ForeignKey('t_movies.id'))
 
   def __repr__(self):
     return '<Images id: {} movie id : {}>'.format(self.id, self.movie_id)
@@ -50,4 +50,3 @@ class MovieImages(db.model):
     self.file_path = data['file_path']
     self.height = data['height']
     self.width = data['width']
-    self.is_posters = data['is_posters']
