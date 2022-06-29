96a83bf9e94b969ad3dd6446b4f0946dee8584a5
daotingc <daotingc@gmail.com>
2022-06-29 19:03:54 +0800

    change some css layout and axios

--- frontend/src/components/SearchComponent.jsx
+++ frontend/src/components/SearchComponent.jsx
@@ -2,31 +2,66 @@ import { Input, Select, Row, Col, Space, Tooltip, Divider } from "antd";
 import { AudioOutlined } from "@ant-design/icons";
 import "../css/SearchComponent.css";
 import { useNavigate } from "react-router-dom";
+import openNotification from "./Notification";
+import { useState } from "react";
+import axios from "axios";
 
 const { Search } = Input;
 const { Option } = Select;
 
 const SearchComponent = ({ updateLoginStatus }) => {
   let navigate = useNavigate();
-  const onSearch = (value) => navigate("/searchresult");
+  const [searchTag, setSearchTag] = useState("name");
+
+  const updateSearchTag = (tag) => {
+    // console.log(tag);
+    setSearchTag(tag);
+  }
+
+  const onSearch = (value) => {
+    console.log(value);
+    if (value === "") {
+      openNotification({
+        "title": "Search Error",
+        "content": "Please enter some keywords"
+      })
+      return;
+    }
+    // todo change url
+    axios
+      .get("/url", {
+        token: "token"
+      })
+      .then(function (response) {
+        console.log(response);
+        // todo change url here
+        // navigate("/");
+      })
+      .catch(function (error) {
+        console.log(error);
+        openNotification({
+          "title": "Search error",
+          "content": error
+        })
+      });
+  };
+
   return (
     <div className="search-input">
       <Divider orientation="left">Search</Divider>
       <Row>
         <Col offset = {1}flex="100px">
-          <Select defaultValue="By movie name">
-            <Option value="By movie name">By movie name</Option>
-            <Option value="By genre">By genre</Option>
-            <Option value="By directors">By directors</Option>
+          <Select defaultValue="By movie name" onChange={updateSearchTag}>
+            <Option value="name">By movie name</Option>
+            <Option value="genre">By genre</Option>
+            <Option value="director">By director</Option>
           </Select>
         </Col>
         <Col span={8} flex="auto">
           <Search
             placeholder="input search text"
             enterButton="Search"
-            size="large"
-            maxLength={9}
-            // suffix={suffix}
+            size="mid"
             onSearch={onSearch}
           />
         </Col>
