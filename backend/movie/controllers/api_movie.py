9df16783a5cb78fcfb09dc7b062e364048652651
Harry <z5270637@ad.unsw.edu.au>
2022-06-29 14:34:31 +1000

    add part of search

--- backend/movie/controllers/api_movie.py
+++ backend/movie/controllers/api_movie.py
@@ -2,4 +2,3 @@ from flask_restx import Namespace
 
 class MovieNs:
     movie_ns = Namespace('Movie', description='Movie related operations')
-    
--- backend/movie/controllers/movie_sys.py
+++ backend/movie/controllers/movie_sys.py
@@ -1,17 +1,19 @@
+from ast import keyword
 from flask import Flask
-from flask_restx import Resource, Api
+from flask_restx import Resource, reqparse
 from flask_restx import Namespace
 from movie import db
 from api_movie import MovieNs
 
+movie_ns = MovieNs.movie_ns
 
-@MovieNs.route('/moviedetails')
+@movie_ns.route('/moviedetails')
 class MovieDetailController(Resource):
-    @MovieNs.response(200, 'Successfully retrieved movie details')
-    @MovieNs.response(400, 'Something went wrong')
-    # @MovieNs.response(404, 'Movie not found')
+    @movie_ns.response(200, 'Successfully retrieved movie details')
+    @movie_ns.response(400, 'Something went wrong')
+    # @movie_ns.response(404, 'Movie not found')
     def get(self):
-        data = MovieNs.payload
+        data = movie_ns.payload
         movie_id = data['movie_id']
         movie_details = db.session.query.filter(id == movie_id).first()
         if movie_details != None:
@@ -19,4 +21,17 @@ class MovieDetailController(Resource):
         else:
             return {'message': 'Movie not found'}, 404
 
-#
+@movie_ns.route('/search')
+class MovieSearchController(Resource):
+    @movie_ns.response(200, 'Successfully find movie')
+    @movie_ns.response(400, 'Something went wrong')
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('keyword', type = str, location = 'args', required = True, help = 'keywords to search')
+        args = parser.parse_args()
+        keyword = args['keyword']
+        if keyword == '':
+            return {'message': 'Keyword is empty, please enter again'}, 400
+
+        
