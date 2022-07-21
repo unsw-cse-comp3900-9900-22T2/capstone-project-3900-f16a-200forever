edaf3ed9ce451d5ffd886402f7ef20bce7017f90
daotingc <daotingc@gmail.com>
2022-07-22 02:42:05 +0800

    add codes for movie review

--- frontend/src/components/MovieReview.jsx
+++ frontend/src/components/MovieReview.jsx
@@ -7,6 +7,7 @@ import {
   Form,
   Input,
   List,
+  Select
 } from "antd";
 import moment from "moment";
 import React, { createElement, useState } from "react";
@@ -16,12 +17,15 @@ import axios from "axios";
 import { UserOutlined } from '@ant-design/icons';
 import SingleReview from "./SingleReview";
 
+const { Option } = Select;
 const { TextArea } = Input;
 
 const MovieReview = ({ id, userInfo, loginStatus }) => {
   const [reviews, setReviews] = useState([]);
   const [reviewsNum, setReviewNum] = useState(0);
   const [pageNum, setPageNum] = useState(1);
+  const [order, setOrder] = useState("descending");
+  const [type, setType] = useState("time");
   const [rating, setRating] = useState(1);
 
   const Editor = ({ onChange, onSubmit, submitting, value }) => (
@@ -49,14 +53,14 @@ const MovieReview = ({ id, userInfo, loginStatus }) => {
     </>
   );
 
-  const getReviews = () => {
+  const getReviews = (pageNum) => {
     axios
       // todo change url here
       .get("http://127.0.0.1:8080/review/sort", {
         params: {
           movie_id: id.replace("id=", ""),
-          type: "time",
-          order: "descending",
+          type: type,
+          order: order,
           num_per_page: 20,
           page: 1,
         },
@@ -140,14 +144,38 @@ const MovieReview = ({ id, userInfo, loginStatus }) => {
           />
         }
       />
+      <Select
+        defaultValue={`descending`}
+        onChange={(value) => {setOrder(value)}}
+      >
+        <Option value="descending">Sort: descending</Option>
+        <Option value="ascending">Sort: ascending</Option>
+      </Select>
+      <Select
+        defaultValue={`time`}
+        onChange={(value) => {setType(value)}}
+      >
+        <Option value="time">time</Option>
+        <Option value="likes">likes</Option>
+        <Option value="unlikes">dislikes</Option>
+      </Select>
+      <Button onClick={() => {getReviews(1)}}>
+        GO!
+      </Button>
       <List
         className="comment-list"
-        header={`${reviewsNum} replie(s)`}
+        header={`All replie(s)`}
         itemLayout="horizontal"
         dataSource={reviews}
         renderItem={(item) => (
           <li>
-            <SingleReview item={item} userInfo={userInfo} loginStatus={loginStatus}></SingleReview>
+            <SingleReview
+              item={item}
+              userInfo={userInfo} 
+              loginStatus={loginStatus}
+            >
+            </SingleReview>
+            {/* {item.rating} */}
           </li>
         )}
       />
--- frontend/src/components/SingleReview.jsx
+++ frontend/src/components/SingleReview.jsx
@@ -2,80 +2,173 @@ import { DislikeFilled, DislikeOutlined, LikeFilled, LikeOutlined } from '@ant-d
 import { Avatar, Comment, Rate, Tooltip } from 'antd';
 import axios from 'axios';
 import React, { createElement, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import openNotification from './Notification';
 
 const SingleReview = ({ item, userInfo, loginStatus }) => {
-	const [likes, setLikes] = useState(item.likes_count);
-  const [dislikes, setDislikes] = useState(item.unlikes_count);
+	const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+  const [rating, setRating] = useState(0);
   const [action, setAction] = useState(null);
+  const [display, setDisplay] = useState(true);
 	const navigate = useNavigate();
 
+  useEffect(() => {
+    setLikes(item.likes_count);
+    setDislikes(item.unlikes_count);
+    setRating(item.rating);
+  }, []);
+
+  const loginAlarm = () => {
+    openNotification({
+      "title": "An error occur",
+      "content": "please login first"
+    })
+  }
+
   const like = () => {
-    setLikes(likes + 1);
-    setAction('liked');
+    if (loginStatus === false) {
+      loginAlarm();
+      return;
+    }
     axios
       .post(`http://127.0.0.1:8080/review/react?review_id=${item.id}&reaction=like`, {
-        email: userInfo["email"],
+        email: userInfo.email,
         token: userInfo.token
       })
       .then(function (response) {
         console.log(response.data);
+        if (response.data.is_remove === 1) {
+          setLikes(item.likes_count + 1);
+          setAction('liked');
+        } else {
+          setLikes(item.likes_count - 1);
+          setAction("none");
+          openNotification({
+            "title": "you removed your like"
+          })
+        }
       })
       .catch(function (error) {
         console.log(error.response.data);
         openNotification({
           "title": "An error occur",
+          "content": error.response.data.message
         })
       });
   };
 
   const dislike = () => {
-    setDislikes(dislikes + 1);
-    setAction('disliked');
+    if (loginStatus === false) {
+      loginAlarm();
+      return;
+    }
+    console.log(likes)
+    console.log(item.id)
+    console.log(item.likes_count)
+    axios
+      .post(`http://127.0.0.1:8080/review/react?review_id=${item.id}&reaction=unlike`, {
+        email: userInfo.email,
+        token: userInfo.token
+      })
+      .then(function (response) {
+        console.log(response.data);
+        if (response.data.is_remove === 1) {
+          setDislikes(item.unlikes_count + 1);
+          setAction('disliked');
+        } else {
+          setDislikes(item.unlikes_count - 1);
+          setAction("none");
+          openNotification({
+            "title": "you removed your dislike"
+          })
+        }
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+        openNotification({
+          "title": "An error occur",
+          "content": error.response.data.message
+        })
+      });
   };
+
+  const delete_review = () => {
+    if (loginStatus === false) {
+      loginAlarm();
+      return;
+    }
+    console.log(item.id);
+    console.log(userInfo.token)
+    axios
+      .delete("http://127.0.0.1:8080/review", {
+        data: {
+          email: userInfo.email,
+          token: userInfo.token,
+          review_id: item.id
+        }
+      })
+      .then(function (response) {
+        console.log(response.data);
+        setDisplay(false)
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+        openNotification({
+          "title": "An error occur when deleting a review",
+          "content": error.response.data.message
+        })
+      });
+  }
 
   const actions = [
     <Tooltip key="comment-basic-like" title="Like">
       <span onClick={like}>
         {createElement(action === 'liked' ? LikeFilled : LikeOutlined)}
-        <span className="comment-action">{likes}</span>
       </span>
     </Tooltip>,
+    <span className="comment-action">{likes}</span>,
     <Tooltip key="comment-basic-dislike" title="Dislike">
       <span onClick={dislike}>
         {React.createElement(action === 'disliked' ? DislikeFilled : DislikeOutlined)}
-        <span className="comment-action">{dislikes}</span>
       </span>
     </Tooltip>,
-    <span key="comment-basic-reply-to">DELETE</span>,
+    <span className="comment-action">{dislikes}</span>,
+    <span onClick={delete_review}>DELETE</span>,
   ];
 	return (
-		<Comment
-      actions={actions}
-      author={<a onClick={() => {navigate("/")}}>{item.user_name}</a>}
-      avatar={
-				<Avatar
-					src={ item.user_image !== null ? item.user_image : "https://joeschmoe.io/api/v1/random" }
-					alt={item.user_name}
-				/>
-			}
-      
-      content={
-        <>
-        
-        <p>{item.review_content}</p>
-        </>
-      }
-      datetime={
-				<Tooltip>
+    <>
+    {
+      display === true ? 
+        <Comment
+        actions={actions}
+        author={<a onClick={() => {navigate("/")}}>{item.user_name}</a>}
+        avatar={
+          <Avatar
+            src={ item.user_image !== null ? item.user_image : "https://joeschmoe.io/api/v1/random" }
+            alt={item.user_name}
+          />
+        }
+        content={
           <>
-          <Rate defaultValue={item.rating}></Rate>
-          <span>{item.created_time}</span>
+          <p>{item.review_content}</p>
           </>
-				</Tooltip>
-			}
-    />
+        }
+        datetime={
+          <Tooltip>
+            <>
+            <Rate disabled defaultValue={item.rating}></Rate>
+            {/* <space></space> */}
+            <span>{item.created_time}</span>
+            </>
+          </Tooltip>
+        }
+      />
+      :
+      null
+    }
+    </>
 	)
 }
 
