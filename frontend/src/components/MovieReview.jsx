796603b1b4f619923cdb3b706d6680b2640d5d60
daotingc <daotingc@gmail.com>
2022-07-21 09:12:42 +0800

    add codes for seperate review

--- frontend/src/components/MovieReview.jsx
+++ frontend/src/components/MovieReview.jsx
@@ -10,75 +10,17 @@ import {
 } from "antd";
 import moment from "moment";
 import React, { createElement, useState } from "react";
-import {
-  DislikeFilled,
-  DislikeOutlined,
-  LikeFilled,
-  LikeOutlined,
-} from "@ant-design/icons";
 import { useEffect } from "react";
 import openNotification from "./Notification";
 import axios from "axios";
 import { UserOutlined } from '@ant-design/icons';
+import SingleReview from "./SingleReview";
 
 const { TextArea } = Input;
 
-const CommentList = ({ comments }) => (
-  <List
-    dataSource={comments}
-    header={`${comments.length} ${comments.length > 1 ? "replies" : "reply"}`}
-    itemLayout="horizontal"
-    renderItem={(props) => <Comment {...props} />}
-  />
-);
-
-const data = [
-  {
-    actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-    author: "Han Solo",
-    avatar: "https://joeschmoe.io/api/v1/random",
-    content: (
-      <p>
-        We supply a series of design principles, practical patterns and high
-        quality design resources (Sketch and Axure), to help people create their
-        product prototypes beautifully and efficiently.
-      </p>
-    ),
-    datetime: (
-      <Tooltip
-        title={moment().subtract(1, "days").format("YYYY-MM-DD HH:mm:ss")}
-      >
-        <span>{moment().subtract(1, "days").fromNow()}</span>
-      </Tooltip>
-    ),
-  },
-  {
-    actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-    author: "Han Solo",
-    avatar: "https://joeschmoe.io/api/v1/random",
-    content: (
-      <p>
-        We supply a series of design principles, practical patterns and high
-        quality design resources (Sketch and Axure), to help people create their
-        product prototypes beautifully and efficiently.
-      </p>
-    ),
-    datetime: (
-      <Tooltip
-        title={moment().subtract(2, "days").format("YYYY-MM-DD HH:mm:ss")}
-      >
-        <span>{moment().subtract(2, "days").fromNow()}</span>
-      </Tooltip>
-    ),
-  },
-];
-
-
 const MovieReview = ({ id, userInfo, loginStatus }) => {
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
-  const [action, setAction] = useState(null);
   const [reviews, setReviews] = useState([]);
+  const [reviewsNum, setReviewNum] = useState(0);
   const [pageNum, setPageNum] = useState(1);
   const [rating, setRating] = useState(1);
 
@@ -109,16 +51,19 @@ const MovieReview = ({ id, userInfo, loginStatus }) => {
   const getReviews = () => {
     axios
       // todo change url here
-      .get("http://127.0.0.1:8080/movie/movie_detail", {
+      .get("http://127.0.0.1:8080/review/sort", {
         params: {
           movie_id: id.replace("id=", ""),
-          page: pageNum,
-          review_num_per_page: 20
+          type: "time",
+          order: "descending",
+          num_per_page: 20,
+          page: 1,
         },
       })
       .then(function (response) {
         console.log(response.data);
-        // setMovieInfo(response.data);
+        setReviews(response.data.reviews);
+        setReviewNum(response.data.total);
       })
       // todo handle error
       .catch(function (error) {
@@ -130,22 +75,9 @@ const MovieReview = ({ id, userInfo, loginStatus }) => {
   }
 
   useEffect(() => {
-    
+    getReviews()
   }, []);
 
-  const like = () => {
-    setLikes(1);
-    setDislikes(0);
-    setAction("liked");
-  };
-
-  const dislike = () => {
-    setLikes(0);
-    setDislikes(1);
-    setAction("disliked");
-  };
-
-  const [comments, setComments] = useState([]);
   const [value, setValue] = useState("");
 
   const handleSubmit = () => {
@@ -166,7 +98,7 @@ const MovieReview = ({ id, userInfo, loginStatus }) => {
     // console.log(id);
     // console.log(userInfo);
     axios
-      .post("http://127.0.0.1:8080/review/review", {
+      .post("http://127.0.0.1:8080/review", {
         movie_id: parseInt(id),
         email: userInfo.email,
         token: userInfo.token,
@@ -192,26 +124,8 @@ const MovieReview = ({ id, userInfo, loginStatus }) => {
     setValue(e.target.value);
   };
 
-  const actions = [
-    <Tooltip key="comment-basic-like" title="Like">
-      <span onClick={like}>
-        {createElement(action === "liked" ? LikeFilled : LikeOutlined)}
-        <span className="comment-action">{likes}</span>
-      </span>
-    </Tooltip>,
-    <Tooltip key="comment-basic-dislike" title="Dislike">
-      <span onClick={dislike}>
-        {React.createElement(
-          action === "disliked" ? DislikeFilled : DislikeOutlined
-        )}
-        <span className="comment-action">{dislikes}</span>
-      </span>
-    </Tooltip>,
-  ];
-
   return (
     <>
-      {comments.length > 0 && <CommentList comments={comments} />}
       <Comment
         avatar={
           <Avatar size="large" icon={<UserOutlined/>} ></Avatar>
@@ -227,34 +141,12 @@ const MovieReview = ({ id, userInfo, loginStatus }) => {
       />
       <List
         className="comment-list"
-        header={`${data.length} replies`}
+        header={`${reviewsNum} replie(s)`}
         itemLayout="horizontal"
-        dataSource={data}
+        dataSource={reviews}
         renderItem={(item) => (
           <li>
-            <Comment
-              actions={actions}
-              author={<a>Han Solo</a>}
-              avatar={
-                <Avatar
-                  src="https://joeschmoe.io/api/v1/random"
-                  alt="Han Solo"
-                />
-              }
-              content={
-                <p>
-                  We supply a series of design principles, practical patterns
-                  and high quality design resources (Sketch and Axure), to help
-                  people create their product prototypes beautifully and
-                  efficiently.
-                </p>
-              }
-              datetime={
-                <Tooltip title={moment().format("YYYY-MM-DD HH:mm:ss")}>
-                  <span>{moment().fromNow()}</span>
-                </Tooltip>
-              }
-            />
+            <SingleReview item={item}></SingleReview>
           </li>
         )}
       />
